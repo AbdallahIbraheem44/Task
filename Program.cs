d57fd6af112f22b81f84510987a2fef7998212d4
AbdallahIbraheem44 <121640900+AbdallahIbraheem44@users.noreply.github.com>
2022-12-29 13:27:49 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+namespace PrimeNumbers
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            double num2 =double.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (double l = num1; l <= num2; l++)
+            {
+                for (double m= 1; m <= l; m++)
+                {
+                    if (l % m == 0)
+                        sum++;
+                }
+                if (sum == 2)
+                {
+                    Console.WriteLine(l);
+                    sum = 0;
+                }
+                else
+                {
+                    sum = 0;
+                }
+            }
+        }
+
+    }
+}
+         
+   
+    
+
+
