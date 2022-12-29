6ff29f22f6969cd654d87128e5d1ecfadc455766
AbdallahIbraheem44 <121640900+AbdallahIbraheem44@users.noreply.github.com>
2022-12-29 13:31:23 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,43 +1,24 @@
 ﻿
-using System;
-namespace PrimeNumbers
+ double X,Y,A,B,sum;
+Console.WriteLine("Please enter the X:");
+X = double.Parse(Console.ReadLine());
+Console.WriteLine("please enter the Y:");
+Y = double.Parse(Console.ReadLine());
+
+Console.WriteLine("The Perfect numbers within the given range :");
+
+for (A = X; A <= Y ;A++)
 {
-    class Program
+    B = 1;
+    sum = 0;
+    while (B < A)
     {
-
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter first number");
-            double num1 = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter second number");
-            double num2 =double.Parse(Console.ReadLine());
-
-            int sum = 0;
-            for (double l = num1; l <= num2; l++)
-            {
-                for (double m= 1; m <= l; m++)
-                {
-                    if (l % m == 0)
-                        sum++;
-                }
-                if (sum == 2)
-                {
-                    Console.WriteLine(l);
-                    sum = 0;
-                }
-                else
-                {
-                    sum = 0;
-                }
-            }
-        }
-
+        if (A % B == 0)
+            sum = sum + B;
+        B++;
     }
+    if (sum == A)
+        Console.WriteLine("{0}", A);
 }
-         
-   
-    
-
+Console.WriteLine(" ");
 
